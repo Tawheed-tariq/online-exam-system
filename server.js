62b5b4f1c95f921066e3fb2e017436812c028f93
Tawheed-Tariq <tavaheed_2022bite008@nitsri.ac.in>
2024-05-30 23:10:53 +0530

    commit

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ app.use('/auth', authRoutes);
 app.use('/exams', examRoutes);
 app.use('/results', resultRoutes);
 
+
+app.get('/abc', async (req, res) => {
+    res.send("we are responding")
+})
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
